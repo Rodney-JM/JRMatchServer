5c49fb05265bc5c3ef9a1456863d67fcf30a3564
Rodney <rondineypatricio@gmail.com>
2024-07-02 13:38:46 -0300

    search methods added, JPQL Querys and bug's fixed

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,25 +1,38 @@
 package br.com.alura.screenmatch.controller;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
-import br.com.alura.screenmatch.model.Serie;
-import br.com.alura.screenmatch.repository.SerieRepository;
+import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
     @Autowired
-    private SerieRepository repositorio;
+    private SerieService servicoSerie;
 
-    @GetMapping("/series")
+    @GetMapping
     public List<SerieDTO> obtemSeries(){
+        return servicoSerie.obterTodasSeries();
+    }
 
-        return repositorio.findAll().stream()
-                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
-                .collect(Collectors.toList());
+    @GetMapping("/top5")
+    public List<SerieDTO> obtemTop5Series(){
+        return servicoSerie.obterTop5Series();
+    }
+
+    @GetMapping("/lancamentos")
+    public List<SerieDTO> obtemLancamentos(){
+        return servicoSerie.obterLancamentos();
+    }
+
+    @GetMapping("/{id}")
+    public SerieDTO obtemSeriePorId(@PathVariable Long id){
+        return servicoSerie.obterPorId(id);
     }
 }
